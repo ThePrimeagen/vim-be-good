08615b2c202a06c0710b4476496c6989ba54f7a0
Brian Ryall <brianryall@fastmail.com>
2020-06-07 16:06:29 -0400

    number game chnages to improve gameplay

--- src/game/number-round.ts
+++ src/game/number-round.ts
@@ -2,8 +2,7 @@ import { IGame } from "./types";
 import { Round } from "./round";
 
 const numberRoundInstructions = [
-    'Use C-a and C-x to solve the math equation',
-    "",
+    'Use C-a and C-x to fix the number on the left side of the math equation',
     ""
 ];
 
@@ -29,24 +28,33 @@ export class NumberRound extends Round {
     }
 
     public async render(game: IGame): Promise<string[]> {
-        this.firstNumber = Math.floor(Math.random() * 10 + 1);
-        this.secondNumber = Math.floor(Math.random() * 10 + 1);
-        let addOrSubtract = "+";
+        this.firstNumber = Math.floor(Math.random() * 5 + 1);
+        this.secondNumber = Math.floor(Math.random() * 5 + 1);
 
+        // To prevent neagtive numbers, which feel wield while playing game
+        if(this.secondNumber > this.firstNumber) {
+            let temp = this.secondNumber;
+            this.secondNumber = this.firstNumber;
+            this.firstNumber = temp;
+        }
+
+        let addOrSubtract = "+";
         if(Math.random() > 0.5) {
             // Addition
             this.answer = this.firstNumber + this.secondNumber;
+            this.winLine  = this.answer + " = " + this.firstNumber + " "
+                + addOrSubtract + " " + this.secondNumber;
         }
         else {
             // Subtraction
             this.answer = this.firstNumber - this.secondNumber;
             addOrSubtract = "-";
-        }
-        this.winLine  = this.answer + " = " + this.firstNumber + " "
+            this.winLine  = this.answer + " = " + this.firstNumber + " "
                 + addOrSubtract + " " + this.secondNumber;
+        }
 
         do {
-            this.fakeAnswer = Math.floor(Math.random() * 10 + 1);
+            this.fakeAnswer = Math.floor(Math.random() * 5 + 1);
         }
         while(this.answer == this.fakeAnswer);
 
