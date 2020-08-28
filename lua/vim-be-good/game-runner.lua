72fada74d5543e3e8fff0e18a4a87d5cb8ee1206
Brian Ryall <brianryall@fastmail.com>
2020-08-28 12:15:57 -0400

    fixed double import

--- lua/vim-be-good/game-runner.lua
+++ lua/vim-be-good/game-runner.lua
@@ -1,6 +1,5 @@
 local bind = require("vim-be-good.bind");
 local GameUtils = require("vim-be-good.game-utils");
-local HjklRound = require("vim-be-good.games.hjkl");
 local RelativeRound = require("vim-be-good.games.relative");
 local HjklRound = require("vim-be-good.games.hjkl");
 local WhackAMoleRound = require("vim-be-good.games.whackamole");
